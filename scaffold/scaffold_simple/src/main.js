24a0c1fc6bc46b03dcba587aa6d7f28a038a93ee
malk <peuple_boy@163.com>
2018-12-13 22:46:39 +0800

    update

--- scaffold/scaffold_simple/src/main.js
+++ scaffold/scaffold_simple/src/main.js
@@ -9,37 +9,39 @@ import store from './store'
 
 // ES6 模板语法：http://es6.ruanyifeng.com/#docs/module#import-%E5%91%BD%E4%BB%A4
 // 若是import 不是 export default 模块，则需要使用花括号包含引入变量，可用 as 为引入变量取别名。可以在一个文件内导出多个目标：方法，变量，模板
-import { Person as propsvue } from "./utils/config/constructor.js"
+import { Person as propsvue } from './utils/config/constructor.js'
 
 //项目不能新增依赖，尝试删掉依赖包，重新npm install
 
-Rem.rem();
-window.native = native;
-window.person = propsvue;
+Rem.rem()
+window.native = native
+window.person = propsvue
 
 //路由的钩子函数
 router.afterEach((to, from, next) => {
-	document.title = to.meta.title;
-});
+  document.title = to.meta.title
+})
 
 /* 2. vue 全局公用函数 - 调用在 vux-plugin.vue
 	1. 如果需要让一个工具函数在每个组件可用，可以把方法挂载到 Vue.prototype上。注册在 main.js 内 Vue.prototype.$method = function () {}；
 	2. 一般建议函数名使用 $ 前缀。像 vue-router 的 $route 和 $router。可考虑传参让挂载方法具有多用性。挂载到 prototype 上是为了方便组件内直接用 this.$method 来使用。
 */
 Vue.prototype.$method = function() {
-	alert('如果你需要让一个工具函数在每个组件可用，可以把方法挂载到 Vue.prototype上。');
+  alert(
+    '如果你需要让一个工具函数在每个组件可用，可以把方法挂载到 Vue.prototype上。'
+  )
 }
-Vue.prototype.$updatetitle = (params) => {
-	alert(document.title + " To " + params);
-	document.title = params;
-};
+Vue.prototype.$updatetitle = params => {
+  alert(document.title + ' To ' + params)
+  document.title = params
+}
 
 // vux可选配置1，添加 Fastclick 移除移动端点击延迟
-const FastClick = require('fastclick');
-FastClick.attach(document.body);
+const FastClick = require('fastclick')
+FastClick.attach(document.body)
 
 new Vue({
-	store: store,
-	router: router,
-	render: h => h(App)
+  store: store,
+  router: router,
+  render: h => h(App)
 }).$mount('#app')
