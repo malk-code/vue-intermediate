6031749327386209646768c63d5ba9c6333b7d69
malk <peuple_boy@163.com>
2018-12-13 16:36:04 +0800

    Create index.vue

---
+++ scaffold/scaffold_simple/src/components/router/NestedSecondViews/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="main">
+    <h2>这里是二级路由主页面</h2>
+    <h4>Nested Second Views：二级路由</h4>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {}
+};
+</script>
+
+<style scoped lang="less">
+.main {
+}
+</style>
