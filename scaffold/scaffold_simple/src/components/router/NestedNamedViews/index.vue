1a591bb325b4cac8b6c70f1beb7ebfa7a86cb9e6
malk <peuple_boy@163.com>
2018-12-13 16:36:06 +0800

    Create index.vue

---
+++ scaffold/scaffold_simple/src/components/router/NestedNamedViews/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="main">
+    <h2>这里是命名路由主页面</h2>
+    <h4>Nested Named Views：命名路由</h4>
+    <p>其实这里属于二级路由，命名路由的配置在router.js和当前页面的父组件（容器）内进行配置</p>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  methods: {},
+  computed: {},
+  watch: {},
+  mounted() {}
+};
+</script>
+
+<style scoped lang="less">
+.main {
+  border: 1px dotted rebeccapurple;
+  margin: 15px;
+  padding: 10px;
+}
+</style>
