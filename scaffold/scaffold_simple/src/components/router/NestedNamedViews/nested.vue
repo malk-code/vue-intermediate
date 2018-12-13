bb24da07e954127a8a0dce31fc23b7c63805887f
malk <peuple_boy@163.com>
2018-12-13 16:35:58 +0800

    Create nested.vue

---
+++ scaffold/scaffold_simple/src/components/router/NestedNamedViews/nested.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="main">
+    这是命名路由的navBar组件
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+
+  },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+
+  },
+  computed: { },
+  watch: { },
+  mounted() {
+
+  },
+}
+
+</script>
+
+<style scoped lang="less">
+
+.main {
+
+}
+</style>
